eaed29f477ab7bc9108482ad65546aca2cb5d268
Ayush soni <141997754+ayushsoni155@users.noreply.github.com>
2024-11-13 11:25:46 +0530

    Add files via upload

--- src/component/jsx/Footer.jsx
+++ src/component/jsx/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
       </div>
 
       <div id="c2">
-        <span>Social Media</span>
+        {/* <span>Social Media</span>
         <a id="inst" href="/">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -54,8 +54,8 @@ const Footer = () => {
             <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
           </svg>
           Twitter-X
-        </a>
-        <span>Contact us</span>
+        </a> */}
+        {/* <span>Contact us</span> */}
         Ayush Soni +91-6266898048 <br />
         Akash Mali +91-9893718536 <br />
         Hitesh Baghel +91-7879025039<br />

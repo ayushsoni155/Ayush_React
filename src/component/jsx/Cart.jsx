1b2f2e68cbbd9f4d53aa83e2b9a8e1543203532d
Ayush soni <141997754+ayushsoni155@users.noreply.github.com>
2024-11-21 16:32:36 +0530

    Update Cart.jsx

--- src/component/jsx/Cart.jsx
+++ src/component/jsx/Cart.jsx
@@ -193,7 +193,7 @@ const Cart = () => {
           <ul className="order-items">
             {order.items.map((item, index) => (
               <li key={index}>
-                Subject Code = {item.subject_code}, Quantity = {item.item_quantity}, Price = ₹{item.item_price}
+                Subject Code = {item.subject_code} (x{item.item_quantity}), Price = ₹{item.item_price}
               </li>
             ))}
           </ul>
@@ -220,7 +220,7 @@ const Cart = () => {
           <ul className="order-items">
             {order.items.map((item, index) => (
               <li key={index}>
-               Subject Code = {item.subject_code}, Quantity = {item.item_quantity}, Price = ₹{item.item_price}
+               Subject Code = {item.subject_code} (x{item.item_quantity}), Price = ₹{item.item_price}
               </li>
             ))}
           </ul>

4bfd9900c62ed3bea4baecf7e2a7d4cedfc42ba6
Ayush soni <141997754+ayushsoni155@users.noreply.github.com>
2024-11-22 16:52:24 +0530

    Add files via upload

--- src/component/jsx/Footer.jsx
+++ src/component/jsx/Footer.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import '../css/Footer.css'; // Import the CSS file for styling
 import { Link } from 'react-router-dom';
@@ -9,15 +8,21 @@ const Footer = () => {
   }
   return (
     <>
+    <span id='mainheading'>Connect with us!</span>
+   
     <div id="contact">
       <div id="c1">
-        <span>Connect with us!</span>
+        
         <img src="/LandingPageimg3.png" alt="Social Media Connect" />
       </div>
 
       <div id="c2">
-        {/* <span>Social Media</span>
-        <a id="inst" href="/">
+         <span>Social Media</span>
+         <a id="telegram" href="https://t.me/Bytewise24">
+        <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="currentColor" class="bi bi-telegram" viewBox="0 0 16 16">
+  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.287 5.906q-1.168.486-4.666 2.01-.567.225-.595.442c-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294q.39.01.868-.32 3.269-2.206 3.374-2.23c.05-.012.12-.026.166.016s.042.12.037.141c-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8 8 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629q.14.092.27.187c.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.4 1.4 0 0 0-.013-.315.34.34 0 0 0-.114-.217.53.53 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09"/>
+</svg>Telegram</a>
+        <a id="inst" href="https://www.instagram.com/bytewise24.pvt/profilecard/?igsh=MTN0eHYybW54ZThsaQ==">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="25"
@@ -30,43 +35,18 @@ const Footer = () => {
           </svg>
           Instagram
         </a>
-        <a id="face" href="/">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="25"
-            height="25"
-            fill="currentColor"
-            className="bi bi-facebook"
-            viewBox="0 0 16 16"
-          >
-            <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
-          </svg>
-          Facebook
-        </a>
-        <a id="twit" href="/">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="25"
-            height="25"
-            fill="currentColor"
-            className="bi bi-twitter-x"
-            viewBox="0 0 16 16"
-          >
-            <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
-          </svg>
-          Twitter-X
-        </a> */}
-        {/* <span>Contact us</span> */}
-        Ayush Soni +91-6266898048 <br />
-        Akash Mali +91-9893718536 <br />
-        Hitesh Baghel +91-7879025039<br />
-        Aarti Solanki +91-8819934644<br />
+        
+        <a id="mail" href="mailto:bytewise24.pvt@gmail.com">
+        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+  <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
+</svg>G-Mail
+        </a> 
       </div>
     </div>
     <footer>
     <p>© 2024 ByteWise24.vercel.app. All Rights Reserved.</p>
     <Link to="/t&c" onClick={scrollToTop}>Terms and conditions</Link>
-    <Link to="/PrivacyPolicy" onClick={scrollToTop}>Privacy Policy</Link>
+    <Link to="/PrivacyPolicy" onClick={scrollToTop}>PrivacyPolicy</Link>
   </footer>
 
       </>

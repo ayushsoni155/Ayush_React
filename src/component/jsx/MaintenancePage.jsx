7abce479a8c085fd63e74ff9571de23ff42a6c63
Ayush soni <141997754+ayushsoni155@users.noreply.github.com>
2024-12-13 17:48:40 +0530

    Add files via upload

--- src/component/jsx/MaintenancePage.jsx
+++ src/component/jsx/MaintenancePage.jsx
@@ -29,11 +29,11 @@ const MaintenancePage = () => {
           viewBox="0 0 100 100"
           fill="none"
         >
-          <circle cx="50" cy="50" r="45" stroke="#4d97e1" strokeWidth="10" />
-          <line x1="50" y1="5" x2="50" y2="25" stroke="#4d97e1" strokeWidth="5" />
-          <line x1="95" y1="50" x2="75" y2="50" stroke="#4d97e1" strokeWidth="5" />
-          <line x1="50" y1="95" x2="50" y2="75" stroke="#4d97e1" strokeWidth="5" />
-          <line x1="5" y1="50" x2="25" y2="50" stroke="#4d97e1" strokeWidth="5" />
+          <circle cx="50" cy="50" r="45" stroke="#FF0000" strokeWidth="10" />
+          <line x1="50" y1="5" x2="50" y2="25" stroke="#FF0000" strokeWidth="5" />
+          <line x1="95" y1="50" x2="75" y2="50" stroke="#FF0000" strokeWidth="5" />
+          <line x1="50" y1="95" x2="50" y2="75" stroke="#FF0000" strokeWidth="5" />
+          <line x1="5" y1="50" x2="25" y2="50" stroke="#FF0000" strokeWidth="5" />
         </svg>
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -41,11 +41,11 @@ const MaintenancePage = () => {
           viewBox="0 0 100 100"
           fill="none"
         >
-          <circle cx="50" cy="50" r="45" stroke="#ff6f61" strokeWidth="10" />
-          <line x1="50" y1="5" x2="50" y2="25" stroke="#ff6f61" strokeWidth="5" />
-          <line x1="95" y1="50" x2="75" y2="50" stroke="#ff6f61" strokeWidth="5" />
-          <line x1="50" y1="95" x2="50" y2="75" stroke="#ff6f61" strokeWidth="5" />
-          <line x1="5" y1="50" x2="25" y2="50" stroke="#ff6f61" strokeWidth="5" />
+          <circle cx="50" cy="50" r="45" stroke="#FFFF00" strokeWidth="10" />
+          <line x1="50" y1="5" x2="50" y2="25" stroke="#FFFF00" strokeWidth="5" />
+          <line x1="95" y1="50" x2="75" y2="50" stroke="#FFFF00" strokeWidth="5" />
+          <line x1="50" y1="95" x2="50" y2="75" stroke="#FFFF00" strokeWidth="5" />
+          <line x1="5" y1="50" x2="25" y2="50" stroke="#FFFF00" strokeWidth="5" />
         </svg>
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -53,11 +53,11 @@ const MaintenancePage = () => {
           viewBox="0 0 100 100"
           fill="none"
         >
-          <circle cx="50" cy="50" r="45" stroke="#ffd700" strokeWidth="10" />
-          <line x1="50" y1="5" x2="50" y2="25" stroke="#ffd700" strokeWidth="5" />
-          <line x1="95" y1="50" x2="75" y2="50" stroke="#ffd700" strokeWidth="5" />
-          <line x1="50" y1="95" x2="50" y2="75" stroke="#ffd700" strokeWidth="5" />
-          <line x1="5" y1="50" x2="25" y2="50" stroke="#ffd700" strokeWidth="5" />
+          <circle cx="50" cy="50" r="45" stroke=" #00ff00" strokeWidth="10" />
+          <line x1="50" y1="5" x2="50" y2="25" stroke=" #00ff00" strokeWidth="5" />
+          <line x1="95" y1="50" x2="75" y2="50" stroke=" #00ff00" strokeWidth="5" />
+          <line x1="50" y1="95" x2="50" y2="75" stroke=" #00ff00" strokeWidth="5" />
+          <line x1="5" y1="50" x2="25" y2="50" stroke=" #00ff00" strokeWidth="5" />
         </svg>
       </div>
       <h1 className="message">Our website is currently under maintenance</h1>

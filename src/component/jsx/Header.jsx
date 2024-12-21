78c96114ce37e7614f3c7b60213643aeef725fa3
Ayush soni <141997754+ayushsoni155@users.noreply.github.com>
2024-12-21 15:28:58 +0530

    Update Header.jsx

--- src/component/jsx/Header.jsx
+++ src/component/jsx/Header.jsx
@@ -83,56 +83,52 @@ const Header = () => {
   };
 
   return (
-    <header>
+ <header>
       <div className="container">
         <div className="logo">
+          {/* Add the logo image here */}
           <Link to="/" onClick={handleLinkClick}>
-            <img
-              src="logo-transparent-png.png"
-              alt="ByteWise Logo"
-              className="logo-img"
-            />
+            <img src="logo-transparent-png.png" alt="ByteWise Logo" className="logo-img" />
           </Link>
           <p id="tagline">Your toolkit for engineering success</p>
         </div>
+
+        {/* Navigation Bar */}
         <nav className={isMenuOpen ? "nav-active" : ""}>
           <ul className="nav-links">
-            <li>
-              <Link to="/" onClick={handleLinkClick}>
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to="/notes" onClick={handleLinkClick}>
-                Notes
-              </Link>
-            </li>
-            <li>
-              <Link to="/lab-manuals" onClick={handleLinkClick}>
-                Lab Manuals
-              </Link>
-            </li>
-            <li>
-              <Link to="/courses" onClick={handleLinkClick}>
-                Courses
-              </Link>
-            </li>
+            <li><Link to="/" onClick={handleLinkClick}>Home</Link></li>
+            <li><Link to="/notes" onClick={handleLinkClick}>Notes</Link></li>
+            <li><Link to="/lab-manuals" onClick={handleLinkClick}>Lab Manuals</Link></li>
+            <li><Link to="/courses" onClick={handleLinkClick}>Courses</Link></li>
           </ul>
-          {loginData ? (
+
+          {/* Conditional rendering for Login/Profile button */}
+          {isLoggedIn ? (
             <button className="profile-btn">
-              <Link to="/profile">Profile</Link>
+              <Link to="/profile">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+                  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                  <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                </svg>
+              </Link>
             </button>
-          ) : (
+          ) : isSignupDone ? (
             <button id="LoginBtn">
               <Link to="/login">Login</Link>
             </button>
+          ) : (
+            <button id="LoginBtn">
+              <Link to="/signup">Signup</Link>
+            </button>
           )}
+
+          {/* Cart Button */}
           <button className="cartprofile">
-            <Link to="/cart" onClick={scrollToTop}>
-              <CiShoppingCart />
-            </Link>
+            <Link to="/cart" onClick={scrollToTop}><CiShoppingCart /></Link>
           </button>
         </nav>
+
+        {/* Menu Icon for Mobile View */}
         <div className="menu-icon" onClick={toggleMenu}>
           <FaBars />
         </div>
@@ -140,5 +136,4 @@ const Header = () => {
     </header>
   );
 };
-
 export default Header;

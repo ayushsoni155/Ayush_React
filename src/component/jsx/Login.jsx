51e471f1d488d06de574efa054f89db7097ecb72
Ayush soni <141997754+ayushsoni155@users.noreply.github.com>
2024-12-25 19:47:02 +0530

    Update Login.jsx

--- src/component/jsx/Login.jsx
+++ src/component/jsx/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
         enrolmentID: "",
         password: "",
     });
-    const [loading,setLoading]= useState(true);
+    const [loading,setLoading]= useState(false);
     const [notification, setNotification] = useState(null);
     const [errors, setErrors] = useState({ enrolmentID: "", password: "" });
     const navigate = useNavigate();

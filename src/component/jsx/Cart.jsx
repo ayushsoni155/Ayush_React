b3a098f44726e9309c773bf0c1a84feeabba5b1e
Ayush soni <141997754+ayushsoni155@users.noreply.github.com>
2024-12-27 21:20:38 +0530

    Update Cart.jsx

--- src/component/jsx/Cart.jsx
+++ src/component/jsx/Cart.jsx
@@ -43,8 +43,8 @@ const Cart = () => {
   const [loadingPay, setLoadingPay] = useState(false);
 
   const totalPrice = cartItems.reduce((total, item) => total + item.sellingPrice * item.quantity, 0);
-  const totalSaveing = cartItems.reduce((totalS, itemS) => totalS + itemS.pages * itemS.quantity, 0);
-  
+  const tempSaveing = cartItems.reduce((totalS, itemS) => totalS + itemS.pages * itemS.quantity, 0);
+  const totalSaveing= totalPrice-tempSaveing;
 
   const fetchOrders = useCallback(async () => {
     if (!isLoggedIn) return;

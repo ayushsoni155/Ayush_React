e03a248e3becde7984e5bb22062e9c52baaf8395
Ayush soni <141997754+ayushsoni155@users.noreply.github.com>
2025-01-14 15:40:25 +0530

    Add files via upload

---
+++ src/videoObj.js
@@ -0,0 +1,250 @@
+const videoObj = [
+  {
+    name: "Engineering Chemistry",
+    Subject_code: "BT101",
+    Sem: "1",
+    branch: "CSE",
+    description: "A beginner-friendly playlist of Engineering Chemistry.",
+    videoUrl: "https://www.youtube.com/embed/CVK0hzlPI3k?si=1JNQrbH7N8Wqt1I2"
+  },
+  {
+    name: "Engineering Mathematics-1",
+    Subject_code: "BT102",
+    Sem: "1",
+    branch: "CSE",
+    description: "Complete Series of Engineering Mathematics-1.",
+    videoUrl: "https://www.youtube.com/embed/bQ_B9cHBYfQ?si=pzQn-FP-6cSRI063"
+  },
+  {
+    name: "English for communication",
+    Subject_code: "BT103",
+    Sem: "1",
+    branch: "CSE",
+    description: "Introduction to Communication Skills.",
+    videoUrl: "https://www.youtube.com/embed/W-4IcNJIyM8?si=k2MDl2_gv_oefswW"
+  },
+{
+    name: "Basic Electrical and Electronics Engineering",
+    Subject_code: "BT104",
+    Sem: "1",
+    branch: "CSE",
+    description: "It covers fundamental concepts of electricity, circuits, electrical machines, and electronic devices..",
+    videoUrl: "https://www.youtube.com/embed/UmOfMNPdIII?si=0gJv1RTEmuWOUxAY"
+  },
+{
+    name: "Engineering Graphics",
+    Subject_code: "BT105",
+    Sem: "1",
+    branch: "CSE",
+    description: "It teaches the basics of technical drawing, including projections, drafting, and visualization of objects",
+    videoUrl: "https://www.youtube.com/embed/K7msuqNcRng?si=yI1EAZkaoM3cCa_Q"
+  },
+{
+    name: "Engineering Physics",
+    Subject_code: "BT201",
+    Sem: "2",
+    branch: "CSE",
+    description: "fundamental concepts of physics",
+    videoUrl: "https://www.youtube.com/embed/NHRoJdRKR0g?si=3byAoDUgrnS0HIKw"
+  },{
+    name: "Engineering Mathematics2",
+    Subject_code: "BT202",
+    Sem: "2",
+    branch: "CSE",
+    description: "Complete series of M2",
+    videoUrl: "https://www.youtube.com/embed/McOc6OUC7Pc?si=T8QrSdKnWT3ZIUor"
+  },{
+    name: "Basic Mechanical Engineering",
+    Subject_code: "BT203",
+    Sem: "2",
+    branch: "CSE",
+    description: "It introduces fundamental concepts of mechanics, thermodynamics, and material science.",
+    videoUrl: "https://www.youtube.com/embed/qDZjRCUBMLg?si=TgoDP5Ftb6eEZF1H"
+  },{
+    name: "Basic Civil Engineering & Mechanics",
+    Subject_code: "BT204",
+    Sem: "2",
+    branch: "CSE",
+    description: "It covers fundamental principles of civil engineering, including structures, materials, and mechanics.",
+    videoUrl: "https://www.youtube.com/embed/Y6AyIevAut8?si=wnPfLM6kWqozWOtt"
+  },{
+    name: "Basic Computer Engineering",
+    Subject_code: "BT205",
+    Sem: "2",
+    branch: "CSE",
+    description: "It essential for understanding how computers and software work.",
+    videoUrl: "https://www.youtube.com/embed/F17KeUrTU0U?si=qfNX9vU8IiFf1QiF"
+  },{
+    name: "Energy & Environmental Engineering",
+    Subject_code: "ES301",
+    Sem: "3",
+    branch: "CSE",
+    description: "It focuses on the study of energy systems, sustainability, and environmental protection.",
+    videoUrl: "https://www.youtube.com/embed/ZwEOVdjUBws?si=vPeejoHhQDyLdLEF"
+  },{
+    name: "Discrete Structure",
+    Subject_code: "CS302",
+    Sem: "3",
+    branch: "CSE",
+    description: "It studies mathematical concepts related to countable, distinct objects, logic, set theory, graph theory, and combinatorics.",
+    videoUrl: "https://www.youtube.com/embed/wGLTV8MgLlA?si=ZYGgm6Uiz3ah7Ant"
+  },{
+    name: "Data Structure",
+    Subject_code: "CS303",
+    Sem: "3",
+    branch: "CSE",
+    description: "It focuses on organizing and storing data efficiently.",
+    videoUrl: "https://www.youtube.com/embed/J0OvDNmAWNw?si=TrmiFNloNViZyGAm"
+  },{
+    name: "Digital Systems",
+    Subject_code: "CS304",
+    Sem: "3",
+    branch: "CSE",
+    description: "It studies about logic gates, number systems, and digital signal processing.",
+    videoUrl: "https://www.youtube.com/embed/pHNbm-4reIc?si=3PvQllaMahb33_T4"
+  },{
+    name: "Object-Oriented Programming & Methodology",
+    Subject_code: "CS305",
+    Sem: "3",
+    branch: "CSE",
+    description: "It emphasizing principles like encapsulation, inheritance, and polymorphism to create modular and reusable code.",
+    videoUrl: "https://www.youtube.com/embed/eWHzz-WHQh4?si=QDHP1VqEw_sdBG7_"
+  },{
+    name: "Mathematics - III (M3)",
+    Subject_code: "CS401",
+    Sem: "4",
+    branch: "CSE",
+    description: "It covers advanced topics such as linear algebra, complex variables, and partial differential equations.",
+    videoUrl: ""
+  },{
+    name: "Analysis and Design of Algorithms",
+    Subject_code: "CS402",
+    Sem: "4",
+    branch: "CSE",
+    description: "It teaches how to evaluate the performance of algorithms and create efficient solutions to problems.",
+    videoUrl: ""
+  },{
+    name: "Software Engineering",
+    Subject_code: "CS403",
+    Sem: "4",
+    branch: "CSE",
+    description: "It focuses on the systematic design, development, testing, and maintenance of software.",
+    videoUrl: ""
+  },{
+    name: "Computer Organization & Architecture",
+    Subject_code: "CS404",
+    Sem: "4",
+    branch: "CSE",
+    description: "It explains how computers are built and how they work, processor, memory, and data handling.",
+    videoUrl: ""
+  },
+  {
+    name: "Operating System",
+    Subject_code: "CS405",
+    Sem: "4",
+    branch: "CSE",
+    description: "It explains how a computer works by managing hardware, running programs, and handling tasks like memory and storage.",
+    videoUrl: ""
+  },
+{
+    name: "Theory of Computation",
+    Subject_code: "CS501",
+    Sem: "5",
+    branch: "CSE",
+    description: "It studies how computers solve problems.",
+    videoUrl: "https://www.youtube.com/embed/acCztqcZi_Q?si=IcA1d-3lU4o4u7wf"
+  },
+  {
+    name: "Database Management Systems",
+    Subject_code: "CS502",
+    Sem: "5",
+    branch: "CSE",
+    description: "Understand the fundamentals of DBMS.",
+    videoUrl: "https://www.youtube.com/embed/jzuzxEFoiss?si=l9xj4fRQblS5UIQR"
+  },
+{
+    name: "Data Analytics",
+    Subject_code: "CS503",
+    Sem: "5",
+    branch: "CSE",
+    description: "It involves analyzing data to find patterns, make decisions, and solve problems using techniques.",
+    videoUrl: "https://www.youtube.com/embed/ByjLuByuK-M?si=s5C_fh8epuFjdbgy"
+  },{
+    name: "Internet and Web Technology",
+    Subject_code: "CS504",
+    Sem: "5",
+    branch: "CSE",
+    description: "It covers how the internet works and how websites are built.",
+    videoUrl: "https://www.youtube.com/embed/RFtGjN5UDvE?si=UJbIMZDdC7vvLo0V"
+  },
+{
+    name: "Machine Learning",
+    Subject_code: "CS601",
+    Sem: "6",
+    branch: "CSE",
+    description: "It teaches computers to learn from data and improve their performance on tasks.",
+    videoUrl: "https://www.youtube.com/embed/Y4qO9unerGs?si=4ruRg0n2qpLouHjc"
+  },{
+    name: "Computer Network",
+    Subject_code: "CS602",
+    Sem: "6",
+    branch: "CSE",
+    description: "It focus on how computers connect and communicate with each other to share data and resources.",
+    videoUrl: "https://www.youtube.com/embed/1V9mhVgVH3A?si=43u-5HBN_nJbno3j"
+  },{
+    name: "Compiler Design",
+    Subject_code: "CS603",
+    Sem: "6",
+    branch: "CSE",
+    description: "It focuses on building programs (compilers) that translate code from one programming language to another.",
+    videoUrl: "https://www.youtube.com/embed/7ICf62fp_4I?si=cvDgGpjfa5DDUCM7"
+  },{
+    name: "Project Management",
+    Subject_code: "CS604",
+    Sem: "6",
+    branch: "CSE",
+    description: "It is about gathering, organizing, and sharing information.",
+    videoUrl: "https://www.youtube.com/embed/Wnk_pArBylM?si=SS38FAF89nEbCJB5"
+  },{
+    name: "Software Architecture",
+    Subject_code: "CS701",
+    Sem: "7",
+    branch: "CSE",
+    description: "It defining how different parts work together to meet requirements and design goals.",
+    videoUrl: "https://www.youtube.com/embed/kQm0meEhQUg?si=CAY6SiICHLW8f3bQ"
+  },{
+    name: "Big Data",
+    Subject_code: "CS702",
+    Sem: "7",
+    branch: "CSE",
+    description: "It refers to large and complex data sets that require special tools to store, analyze, and manage the data sets.",
+    videoUrl: "https://www.youtube.com/embed/VYe4o_kHMZ8?si=xzb78RwWWJdx5CWA"
+  },
+{
+    name: "Internet of Things",
+    Subject_code: "CS801",
+    Sem: "8",
+    branch: "CSE",
+    description: "It is about connects devices to the internet to improve monitoring and control.",
+    videoUrl: "https://www.youtube.com/embed/cKYgZcTjzk0?si=dxoFYzueopeqDl4E"
+  },
+{
+    name: "Cloud Computing",
+    Subject_code: "CS802",
+    Sem: "8",
+    branch: "CSE",
+    description: "It teaches how to store and access data and applications online.",
+    videoUrl: "https://www.youtube.com/embed/YgIA0S16I2o?si=4nyFRcw-xHHRoK-p"
+  },
+{
+    name: "Game Theory with Engineering Applications",
+    Subject_code: "CS803",
+    Sem: "8",
+    branch: "CSE",
+    description: "It studies strategies for decision-making in competitive situations.",
+    videoUrl: "https://www.youtube.com/embed/YgqC8_WOZyY?si=wLMJas9mhzpaJBNr"
+  },
+];
+  
+  export default videoObj;
+  
